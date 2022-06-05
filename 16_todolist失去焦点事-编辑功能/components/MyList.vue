c037356e452ec2f759787aa22f1103dcdcb9379a
yangbaixue <limingzhangpy@163.com>
2022-06-05 12:55:17 +0800

    todolist 失去焦点事件触发更新标题，替代确认按钮

---
+++ 16_todolist失去焦点事-编辑功能/components/MyList.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul class="todo-main">
+        <MyItem 
+        v-for="todoObj of todos" 
+        :key="todoObj.id" 
+        :todo="todoObj"/> 
+    </ul>
+</template>
+
+<script>
+import MyItem from '@/components/MyItem'
+export default {
+    name:"MyList",
+    components:{MyItem},
+    props:["todos"]
+}
+</script>
+
+<style>
+
+</style>
