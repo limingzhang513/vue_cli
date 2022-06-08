3f80d96f4108bb6f0fbfe15183721b39092e4da0
yangbaixue <limingzhangpy@163.com>
2022-06-08 16:44:16 +0800

    vue-resource

---
+++ 21_github搜索，vue-resource发送请求/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <Search/>
+        <List/> 
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import List from './components/List'
+import Search from './components/Search'
+export default {
+    components: {List, Search},
+    name:"App",
+}
+</script>
+<style>
+</style>
